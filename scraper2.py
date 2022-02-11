fa85b4fcf51f0e27026e23e9f719b7b0f7e151ca
Libby Duggan <libby.duggan@gmail.com>
2022-02-10 20:44:47 -0500

    DB recipe code

--- scraper2.py
+++ scraper2.py
@@ -2,6 +2,14 @@ from imp import is_builtin
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import psycopg2
+
+#establishing the connection
+conn = psycopg2.connect(
+   database="", user='postgres', password='', host='127.0.0.1', port= '5432'
+)
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
 
 url = 'https://www.homebrewersassociation.org/homebrew-recipe/double-black-imperial-black-ipa/'
 html = urllib.request.urlopen(url).read()
